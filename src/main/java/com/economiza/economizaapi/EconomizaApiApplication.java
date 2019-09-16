f54132313be29d5966f9b43b8fc1f9ee66ef60c0
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-09-16 14:00:23 -0300

    first commit

---
+++ src/main/java/com/economiza/economizaapi/EconomizaApiApplication.java
@@ -0,0 +1,13 @@
+package com.economiza.economizaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomizaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomizaApiApplication.class, args);
+	}
+
+}
