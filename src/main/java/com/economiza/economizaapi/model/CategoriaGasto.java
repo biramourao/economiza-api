3263d5ed3b67780e9c46acb0d29bcafd91eae281
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-09-16 17:42:55 -0300

    Começo da implementacao.

---
+++ src/main/java/com/economiza/economizaapi/model/CategoriaGasto.java
@@ -0,0 +1,15 @@
+package com.economiza.economizaapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class CategoriaGasto {
+
+	private String descricao;
+	private int cod;
+}
---
+++ src/main/java/com/economiza/economizaapi/model/Gasto.java
@@ -0,0 +1,15 @@
+package com.economiza.economizaapi.model;
+
+import java.util.Date;
+
+public class Gasto {
+	
+	private int cod;
+	private String nome;
+	private double valor;
+	private Date vencimento;
+	private String tipo;
+	private int qtdParcelas;
+	private String status;
+
+}
