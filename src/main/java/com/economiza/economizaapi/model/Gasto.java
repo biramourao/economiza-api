0aaa97dd5b0b1185023c22f07f7eb6ba0f34a2d6
Bira <Bira@192.168.1.7>
2019-09-19 23:44:41 -0300

    Implementação das classes (sem JPA)

--- src/main/java/com/economiza/economizaapi/model/Gasto.java
+++ src/main/java/com/economiza/economizaapi/model/Gasto.java
@@ -2,6 +2,14 @@ package com.economiza.economizaapi.model;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
 public class Gasto {
 	
 	private int cod;
@@ -11,5 +19,8 @@ public class Gasto {
 	private String tipo;
 	private int qtdParcelas;
 	private String status;
+	private Usuario usuario;
+	private CategoriaGasto categoriaGasto;
+	private CartaoDeCredito cartaoDeCredito;
 
 }
