6186a21766a646d2c5bb6ec41cb1cba79de310f8
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-09-21 00:21:39 -0300

    Criação da FonteDeRendaRepository.java

---
+++ src/main/java/com/economiza/economizaapi/repository/FonteDeRendaRepository.java
@@ -0,0 +1,9 @@
+package com.economiza.economizaapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.economiza.economizaapi.model.FonteDeRenda;
+
+public interface FonteDeRendaRepository extends JpaRepository<FonteDeRenda, Long>{
+	
+}
