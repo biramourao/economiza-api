52a23966f390d05041e409311b1167a5c45c9aed
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-09-20 22:54:19 -0300

    Criação do UsuarioRepository.java

---
+++ src/main/java/com/economiza/economizaapi/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.economiza.economizaapi.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.economiza.economizaapi.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String>{
+
+	public Optional<Usuario> getUsuarioByEmailAndSenha(String email, String senha);
+}
