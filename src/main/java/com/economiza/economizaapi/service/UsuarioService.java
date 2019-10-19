f6e933a162a8b550c2cc0ea18287a486df9641b1
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-10-19 18:30:04 -0300

    Implementação da interface UserDetailsService
    Para a carregar dados de usuario via username

--- src/main/java/com/economiza/economizaapi/service/UsuarioService.java
+++ src/main/java/com/economiza/economizaapi/service/UsuarioService.java
@@ -1,9 +1,16 @@
 package com.economiza.economizaapi.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.economiza.economizaapi.exception.NotFoundException;
@@ -13,7 +20,7 @@ import com.economiza.economizaapi.service.util.HashUtil;
 
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService{
 @Autowired private UsuarioRepository usuarioRepository;
 	
 	public Usuario save(Usuario user) {
@@ -51,5 +58,16 @@ public class UsuarioService {
 	}
 	public void deleteById(Long id) {
 		usuarioRepository.deleteById(id);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Usuario> resultado = usuarioRepository.findByEmail(username);
+		if (!resultado.isPresent()) throw new UsernameNotFoundException("Não existe usuário com esse email: "+username);
+			
+		Usuario usuario = resultado.get();
+		List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE BASIC"));
+		User user  = new User(usuario.getEmail(), usuario.getSenha(), authorities);
+		return user;
 	}
 }
