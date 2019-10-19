deb52e80be900fa2f07b73ff08dbe5684298e2c6
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-10-19 18:49:35 -0300

    Exposição do AuthenticationManager como um Bean do Spring

--- src/main/java/com/economiza/economizaapi/security/SecurityConfig.java
+++ src/main/java/com/economiza/economizaapi/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.economiza.economizaapi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.economiza.economizaapi.service.UsuarioService;
 
@@ -15,8 +19,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(usuarioService);
+		auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder);
+	}
+	
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)	
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
 	}
 }
