9f97552fc21fb7f2a2176656e87cba57cb9b22f4
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-10-24 17:16:17 -0300

    Create SecurityConstants.java

---
+++ src/main/java/com/economiza/economizaapi/constant/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.economiza.economizaapi.constant;
+
+public class SecurityConstants {
+	public static final int JWT_EXP_DAYS = 5;
+	public static final String API_KEY = "ec0n0miz4-4p1";
+	public static final String JWT_PROVIDER = "Bearer";
+	public static final String JWT_ROLE_KEY = "role";
+}
