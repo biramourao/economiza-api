fee33152a4bfb10e241862b74b8c2b6cf9fc256c
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-10-25 00:55:52 -0300

    Proteção dos Resources da API

--- src/main/java/com/economiza/economizaapi/security/SecurityConfig.java
+++ src/main/java/com/economiza/economizaapi/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.economiza.economizaapi.service.UsuarioService;
 
@@ -33,11 +35,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)	
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
-		// TODO Auto-generated method stub
 		return super.authenticationManagerBean();
 	}
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers(HttpMethod.POST, "/usuarios/login");
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				   .authorizeRequests()
+				   .anyRequest().authenticated();
+		http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 }
