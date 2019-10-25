055eef90e80e51231aed801a0151a249709e667d
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-10-24 22:38:25 -0300

    Criação do ouvinte de BadCredencialException para que o erro seja enviado quando estiver incorreto o login

--- src/main/java/com/economiza/economizaapi/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/economiza/economizaapi/resource/exception/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,6 +21,13 @@ public class ResourceExceptionHandler {
 		ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex) {
+		ApiError error = new ApiError(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), new Date());
+
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
 	}
 	
 	@ExceptionHandler(ConstraintViolationException.class)
