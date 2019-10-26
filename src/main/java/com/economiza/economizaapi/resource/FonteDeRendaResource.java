71f1b556899761b9e982a471683a1148c9dfca74
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-10-25 22:06:24 -0300

    Implementação da verificação de usuário no FonteDeRendaResource

--- src/main/java/com/economiza/economizaapi/resource/FonteDeRendaResource.java
+++ src/main/java/com/economiza/economizaapi/resource/FonteDeRendaResource.java
@@ -1,10 +1,13 @@
 package com.economiza.economizaapi.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +19,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.economiza.economizaapi.exception.NotFoundException;
 import com.economiza.economizaapi.model.FonteDeRenda;
-import com.economiza.economizaapi.repository.FonteDeRendaRepository;
+import com.economiza.economizaapi.model.Usuario;
+import com.economiza.economizaapi.repository.UsuarioRepository;
 import com.economiza.economizaapi.service.FonteDeRendaService;
 
 @RestController
 @RequestMapping("/fontes-de-renda")
 public class FonteDeRendaResource {
 	@Autowired private FonteDeRendaService fonteDeRendaService;
-	@Autowired private FonteDeRendaRepository fonteDeRendaRepository;
+	@Autowired private UsuarioRepository usuarioRepository;
 
 	@CrossOrigin
 	@PostMapping
@@ -34,6 +39,7 @@ public class FonteDeRendaResource {
 	}
 
 	@CrossOrigin
+	@PreAuthorize("@accessManager.usuarioDaFonteDeRenda(#cod)")
 	@PutMapping("/{cod}")
 	public ResponseEntity<FonteDeRenda> update(@PathVariable(name = "cod") Long cod, @RequestBody FonteDeRenda fonteDeRenda){
 		fonteDeRenda.setCod(cod);
@@ -42,21 +48,16 @@ public class FonteDeRendaResource {
 	}
 
 	@CrossOrigin
+	@PreAuthorize("@accessManager.usuarioDaFonteDeRenda(#cod)")
 	@GetMapping("/{cod}")
 	public ResponseEntity<FonteDeRenda> getById(@PathVariable("cod") Long cod) {
 		FonteDeRenda fonteDeRenda = fonteDeRendaService.getById(cod);
 		return ResponseEntity.ok(fonteDeRenda);
 	}
-
-	@CrossOrigin
-	@GetMapping
-	public ResponseEntity<List<FonteDeRenda>> listAll() {
-		List<FonteDeRenda> fontesDeRenda = fonteDeRendaRepository.findAll();
-		return ResponseEntity.ok(fontesDeRenda);
-	}
 	
 	@SuppressWarnings("rawtypes")
 	@CrossOrigin
+	@PreAuthorize("@accessManager.usuarioDaFonteDeRenda(#cod)")
 	@DeleteMapping("/{cod}")
 	@ResponseBody
 	public ResponseEntity delete(@PathVariable(name = "cod") Long cod) {
@@ -71,9 +72,15 @@ public class FonteDeRendaResource {
 	}
 	
 	@CrossOrigin
-	@GetMapping("/usuario/{cod}")
-	public ResponseEntity<List<FonteDeRenda>> getByUserId(@PathVariable("cod") Long cod) {
-		List<FonteDeRenda> fonteDeRenda = fonteDeRendaService.findByUsuarioCod(cod);
+	@GetMapping
+	public ResponseEntity<List<FonteDeRenda>> getByUserId() {
+		String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Optional<Usuario> result = usuarioRepository.findByEmail(email);
+		if(!result.isPresent())  {
+			throw new NotFoundException("Não foi encontrado um usuário com esse email = " + email);
+		}
+		Usuario usuario = result.get();
+		List<FonteDeRenda> fonteDeRenda = fonteDeRendaService.findByUsuarioCod(usuario.getCod());
 		return ResponseEntity.ok(fonteDeRenda);
 	}
 	
