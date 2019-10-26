b0bb024003db4a8be341a69c2f3e2344a034a62f
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-10-25 22:06:01 -0300

    Implementação da verificação de usuário no CartaoDeCreditoResource

--- src/main/java/com/economiza/economizaapi/resource/CartaoDeCreditoResource.java
+++ src/main/java/com/economiza/economizaapi/resource/CartaoDeCreditoResource.java
@@ -1,10 +1,13 @@
 package com.economiza.economizaapi.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +18,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.economiza.economizaapi.exception.NotFoundException;
 import com.economiza.economizaapi.model.CartaoDeCredito;
-import com.economiza.economizaapi.repository.CartaoDeCreditoRepository;
+import com.economiza.economizaapi.model.Usuario;
+import com.economiza.economizaapi.repository.UsuarioRepository;
 import com.economiza.economizaapi.service.CartaoDeCreditoService;
 
 @RestController
 @RequestMapping("/cartoes-de-credito")
 public class CartaoDeCreditoResource {
 	@Autowired private CartaoDeCreditoService cartaoDeCreditoService;
-	@Autowired private CartaoDeCreditoRepository cartaoDeCreditoRepository;
+	@Autowired private UsuarioRepository usuarioRepository;
 
 	@CrossOrigin
 	@PostMapping
@@ -34,6 +38,7 @@ public class CartaoDeCreditoResource {
 	}
 
 	@CrossOrigin
+	@PreAuthorize("@accessManager.usuarioDoCartaoDeCredito(#cod)")
 	@PutMapping("/{cod}")
 	public ResponseEntity<CartaoDeCredito> update(@PathVariable(name = "cod") Long cod, @RequestBody CartaoDeCredito cartaoDeCredito){
 		cartaoDeCredito.setCod(cod);
@@ -42,20 +47,16 @@ public class CartaoDeCreditoResource {
 	}
 
 	@CrossOrigin
+	@PreAuthorize("@accessManager.usuarioDoCartaoDeCredito(#cod)")
 	@GetMapping("/{cod}")
 	public ResponseEntity<CartaoDeCredito> getById(@PathVariable("cod") Long cod) {
 		CartaoDeCredito cartaoDeCredito = cartaoDeCreditoService.getById(cod);
 		return ResponseEntity.ok(cartaoDeCredito);
 	}
 
-	@CrossOrigin
-	@GetMapping
-	public ResponseEntity<List<CartaoDeCredito>> listAll() {
-		List<CartaoDeCredito> categoriaGasto = cartaoDeCreditoRepository.findAll();
-		return ResponseEntity.ok(categoriaGasto);
-	}
 	@SuppressWarnings("rawtypes")
 	@CrossOrigin
+	@PreAuthorize("@accessManager.usuarioDoCartaoDeCredito(#cod)")
 	@DeleteMapping("/{cod}")
 	@ResponseBody
 	public ResponseEntity delete(@PathVariable(name = "cod") Long cod) {
@@ -69,9 +70,15 @@ public class CartaoDeCreditoResource {
 		}
 	}
 	@CrossOrigin
-	@GetMapping("/usuario/{cod}")
-	public ResponseEntity<List<CartaoDeCredito>> getByUserId(@PathVariable("cod") Long cod) {
-		List<CartaoDeCredito> cartoesDeCredito = cartaoDeCreditoService.FindByUsuarioCod(cod);
+	@GetMapping
+	public ResponseEntity<List<CartaoDeCredito>> getByUserId() {
+		String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Optional<Usuario> result = usuarioRepository.findByEmail(email);
+		if(!result.isPresent())  {
+			throw new NotFoundException("Não foi encontrado um usuário com esse email = " + email);
+		}
+		Usuario usuario = result.get();
+		List<CartaoDeCredito> cartoesDeCredito = cartaoDeCreditoService.FindByUsuarioCod(usuario.getCod());
 		return ResponseEntity.ok(cartoesDeCredito);
 	}
 }
